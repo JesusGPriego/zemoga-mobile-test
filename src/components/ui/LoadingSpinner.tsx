17ae568649319205ea88e3658affbc73c53e7c36
Jesus G. Priego <70105185+JesusGPriego@users.noreply.github.com>
2023-08-14 17:52:26 +0200

    spinner restyling

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, ActivityIndicator, StyleSheet } from 'react-native';
 import { Colors } from '../../constants';
+import { Dimensions } from 'react-native';
 
 interface Props {
   color?: string;
 }
 
+var { width, height } = Dimensions.get('window');
 export const LoadingSpinner = ({ color = Colors.primary700 }: Props) => {
   return (
     <View style={styles.loading}>
@@ -15,8 +17,15 @@ export const LoadingSpinner = ({ color = Colors.primary700 }: Props) => {
 };
 const styles = StyleSheet.create({
   loading: {
-    flex: 1,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    opacity: 0.5,
+    height: height,
+    width: width,
+    backgroundColor: Colors.primary400,
     justifyContent: 'center',
     alignItems: 'center',
+    zIndex: 9000,
   },
 });
